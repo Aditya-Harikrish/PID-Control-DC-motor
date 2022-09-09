5ba4ac93c00eacbb5b852eff300f67f062b4c7ae
LokeshVekatachalam <lokesh.v@research.iiit.ac.in>
2022-09-10 00:46:53 +0530

    half-commited

--- Microcontroller-code/microcontroller/microcontroller.ino
+++ Microcontroller-code/microcontroller/microcontroller.ino
@@ -1,3 +1,4 @@
+// includes
 
 #include "mbedtls/base64.h"
 #include "mbedtls/md.h"
@@ -9,66 +10,80 @@
 #include <PubSubClient.h>
 #include <ThingSpeak.h>
 #include <WebServer.h>
-char ssid[] = "hackiiit";
-char password[] = "12344321";
-const char* server ="mqtt3.thingspeak.com"; 
-const char mqttUserName[] = "MyAGLRksBQQGFhoLEC0NAx8"; 
-const char mqttPass[] = "YCf3gwLtZG8kYglK/5/gTvGE";
-const char clientID[] = "MyAGLRksBQQGFhoLEC0NAx8";
-unsigned long writeChannelID = 1825221;
-char writeAPIKey[] = "XU89SKWS3ZIIHP52";
-const char* readAPIKey = "U29GEZH7BZ4ECF53";
 
-HTTPClient http;
+// Details
 
-//String topicString = "channels/" + String(writeChannelID) + "/publish";
-//String f1 = "field1=";
-//String f2 = "&field2=";
-//String f3 = "&field3=";
-//String f4 = "&status=MQTTPUBLISH";
-String final_1 = "";
-String topic_1 = "{\n\"write_api_key\": \"XU89SKWS3ZIIHP52\",\n\"updates\": [\n";
-String Temp = "";
-int delta_pt =0 ;
+  // Pin Details
+  
+    int IN1 = 27;     // Input Pin 1 [motor driver]
+    int IN2 = 26;     // Input Pin   [motor driver]
+    int PWM = 14;     // PWM Pin     [motor driver]
+    int ENCA = 32;    // Encoder A   [DC motor]
+    int ENCB = 34;    // Encoder B   [DC motor]
+  
+  // Wifi
+    char ssid[] = "hackiiit";
+    char password[] = "12344321";
+  
+  // Thingspeak 
+    unsigned long writeChannelID = 1825221;
+    char writeAPIKey[] = "XU89SKWS3ZIIHP52";
+    const char* readAPIKey = "U29GEZH7BZ4ECF53";
 
+// Clients
 
-WiFiClient client1;
-PubSubClient mqttClient(server, 1883, client1);
+  HTTPClient http;
+  WiFiClient client1;
 
+// Variables
 
-int IN1 = 27;
-int IN2 = 26;
-int PWM = 14;
-int ENCA = 32;
-int ENCB = 34;
+  // PID constants (can be taken as input also)
+    float k_p = 10;
+    float k_d = 0.025;
+    float k_i = 5;
 
-volatile int posi = 0;
-long prevT = 0;
-float eprev = 0;
-float eintegral = 0;
+  // Strings for json formatting
+    String topic_1 = "{\n\"write_api_key\": \"XU89SKWS3ZIIHP52\",\n\"updates\": [\n";
+    String Temp = "";  
+    String jsonBuffer = ""; //JSON Body
 
+  // Time
+    #define PID_TIMER 10000
+    
+    uint wifi_delay = 5000;
+    uint startTime;
+    uint lastTime;
+    uint lastpubTime;
+    
+    int delta_pt =0 ;
+    int SAMPLING_RATE = 100;
+    long prevT = 0;
+    long currT;
+    float deltaT;
+    
+  // PID variables
+    int dir;
+    int md = 0;
+    bool use_integral = false;
+    float eprev = 0;
+    float eintegral = 0;
+    float e;
+    float dedt;
+    float u;
+    float pwr;
+    volatile int posi = 0;
+ 
+  // Angles
+    float input_target;
+    float previous_target=0.0;
+    float pos = 0;
 
-uint wifi_delay = 5000;
-#define PID_TIMER 10000
-int SAMPLING_RATE = 100;
-
-float input_target;
-float previous_target=0.0;
-
-// PID constants
-float k_p = 10;
-float k_d = 0.025;
-float k_i = 5;
-
-//char jsonBuffer[6500] = "";
-String jsonBuffer = "";
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600);
 
     pinMode(ENCA,INPUT);
     pinMode(ENCB,INPUT);
-//    pinMode(PWM,OUTPUT);
     attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
 
     analogWriteResolution(PWM, 8);
@@ -79,14 +94,10 @@ void setup() {
     Serial.println("Connecting");
     while(WiFi.status() != WL_CONNECTED)
         delay(500);
+    
     Serial.print("\nConnected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
 
-    
-//    Serial.println(WiFi.localIP());
-//    mqttClient.setServer(server,1883);
-//    mqttconnectloop();
-    
-//    delay(1000); 
 //  ThingSpeak.begin(client1);
 
 }
@@ -94,7 +105,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  input_target = 180;
+  input_target = 45.00;
   if(input_target != previous_target){
     PID_control(input_target,k_p,k_i,k_d);
     previous_target = input_target;
@@ -123,34 +134,35 @@ void loop() {
 void PID_control(float target,float kp,float ki,float kd) 
 {
     
-    uint startTime = millis();
-    uint lastTime = millis();
-    uint lastpubTime = millis();
-    bool use_integral = false;
-    int md = 0;
+    startTime = millis();
+    lastTime = millis();
+    lastpubTime = millis();
+    use_integral = false;
+    md = 0;
     
-    float pos = 0;
     jsonBuffer = topic_1;
     while(millis()- startTime < PID_TIMER){
 
       if(millis() - lastTime > SAMPLING_RATE){
 //        setMotor(0,0,PWM,IN1,IN2);
         // time difference
-        long currT = micros();
-        float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+        currT = micros();
+        deltaT = ((float) (currT - prevT))/( 1.0e6 );
         prevT = currT;
 
-        // Read the positionL
-    //    float pos = 0; 
-    //    noInterrupts(); // disable interrupts temporarily while reading
-    //    pos = (float)posi*0.85714285714//* 0.64056939501  ;
-    //    interrupts(); // turn interrupts back on
-        pos = pos + 5;
+        //Read the position
+        pos = 0; 
+        
+        noInterrupts(); // disable interrupts temporarily while reading
+        pos = (float)posi*0.85714285714//* 0.64056939501  ;
+        interrupts(); // turn interrupts back on
+        
+//        pos = pos + 5;
         // error
-        float e = target - pos;
+        e = target - pos;
 
         // derivative
-        float dedt = (e-eprev)/(deltaT);
+        dedt = (e-eprev)/(deltaT);
 
         // integral
         if(dedt == 0)
@@ -175,16 +187,16 @@ void PID_control(float target,float kp,float ki,float kd)
         }
 
         // control signal
-        float u = kp*e + kd*dedt + ki*eintegral;
+        u = kp*e + kd*dedt + ki*eintegral;
 
         // motor power
-        float pwr = fabs(u);
+        pwr = fabs(u);
         if( pwr > 255 ){
             pwr = 255;
         }
 
         // motor direction
-        int dir = 1;
+        dir = 1;
         if(u<0){
             dir = -1;
         }
@@ -208,50 +220,38 @@ void PID_control(float target,float kp,float ki,float kd)
         
         lastTime = millis();
         delta_pt = int((lastTime - lastpubTime)/float(100));
+        
         Temp="{\"delta_t\":\""+String(delta_pt)+"\",";
-        //strcat(jsonBuffer,Temp);
         jsonBuffer += Temp;
+        
         Temp="\"field1\":\""+String(target)+"\",";
-//        strcat(jsonBuffer,Temp);
         jsonBuffer += Temp;
 
         Temp="\"field2\":\""+String(pos)+"\",";
-//      strcat(jsonBuffer,Temp);
         jsonBuffer += Temp;
+        
         Temp="\"field3\":\""+String(u)+"\"";
-//      strcat(jsonBuffer,Temp);
         jsonBuffer += Temp;
+        
         Temp="},\n";
-//-        strcat(jsonBuffer,Temp);
         jsonBuffer += Temp;
+        
         if(fabs(target-pos)<0.5){
           break;
         }
-//        final_1 = f1 + String(target) + f2 + String(pos) + f3 +String(u) + f4;
-//        if(lastTime-lastpubTime > 1500){
-//             if(!mqttClient.connected()) {mqttconnectloop(); }
-//             mqttClient.publish(topicString.c_str(), final_1.c_str());
-//             lastpubTime = lastTime;
-//             Serial.print(topicString.c_str());
-//             Serial.print(" ");
-//             Serial.print(final_1.c_str());
-//             Serial.print(" ");
-//             Serial.println();  
-//        }
         
       }
       
    }
+   setMotor(0,0,PWM,IN1,IN2);
+   // Removing the final ','
    jsonBuffer[jsonBuffer.length()-2] = '\n';
    jsonBuffer[jsonBuffer.length()-1] = ']';
-
-   //jsonBuffer.pop_back();
-   //jsonBuffer += "\n";
+   
    Temp="}";
-//    strcat(jsonBuffer,Temp);
    jsonBuffer += Temp;
-   //Serial.print(jsonBuffer);
-   //Serial.println();  
+
+   // Doing a bulk update
    thingspeakPOST(jsonBuffer);
    
 }
@@ -260,12 +260,11 @@ void thingspeakPOST(String rep) {
   http.begin("https://api.thingspeak.com/channels/1825221/bulk_update.json");
 
   http.addHeader("Content-Type", "application/json");
-  // String req_data = "{\"m2m:cin\": {\"con\": \"" + data + "\",\"cnf\": \"" + description + "\"}}";
-  // Serial.println(req_data);
+  
   Serial.println(rep);
 
   int code = http.POST(rep);
-  if(code > 0)
+  if(code == 202)
   {
     Serial.println(code);
     Serial.println("Successfully added the reading");
@@ -303,3 +302,40 @@ void readEncoder(){
         posi--;
     }
 }
+
+
+
+/*
+ * const char* server ="mqtt3.thingspeak.com"; 
+ * const char mqttUserName[] = "MyAGLRksBQQGFhoLEC0NAx8"; 
+ * const char mqttPass[] = "YCf3gwLtZG8kYglK/5/gTvGE";
+ * const char clientID[] = "MyAGLRksBQQGFhoLEC0NAx8";
+ * String topicString = "channels/" + String(writeChannelID) + "/publish";
+ *   PubSubClient mqttClient(server, 1883, client1);
+ * String f1 = "field1=";
+ * String f2 = "&field2=";
+ * String f3 = "&field3=";
+ * String f4 = "&status=MQTTPUBLISH";
+ * //        final_1 = f1 + String(target) + f2 + String(pos) + f3 +String(u) + f4;
+//        if(lastTime-lastpubTime > 1500){
+//             if(!mqttClient.connected()) {mqttconnectloop(); }
+//             mqttClient.publish(topicString.c_str(), final_1.c_str());
+//             lastpubTime = lastTime;
+//             Serial.print(topicString.c_str());
+//             Serial.print(" ");
+//             Serial.print(final_1.c_str());
+//             Serial.print(" ");
+//             Serial.println();  
+//        }
+String final_1 = "";
+//    mqttClient.setServer(server,1883);
+//    mqttconnectloop();
+    
+//    delay(1000); 
+//    pinMode(PWM,OUTPUT);
+
+//char jsonBuffer[6500] = "";
+ 
+ // String req_data = "{\"m2m:cin\": {\"con\": \"" + data + "\",\"cnf\": \"" + description + "\"}}";
+  // Serial.println(req_data);
+  */ 
